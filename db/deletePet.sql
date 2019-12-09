9006fe7786d6dbc3668cf7c7edc1dddd73f022c4
Josef Walls <josef.l.walls@gmail.com>
2019-12-09 09:07:11 -0600

    Added pet and walker stuff

---
+++ db/deletePet.sql
@@ -0,0 +1,2 @@
+DELETE FROM pets
+WHERE pet_id = $1
--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express()
 
 //controllers
 
-const auth = require("./authController")
+const auth = require("./controllers/authController")
+const pet = require("./controllers/petController")
+const walker = require("./controllers/walkerController")
 
 massive(process.env.CONNECTION_STRING)
 .then(dbInstance => {
@@ -29,5 +31,18 @@ app.use(express.json())
 //auth
 app.post("/Chipper/Register", auth.registerUser)
 app.post("/Chipper/Login", auth.loginUser)
+
+//pet
+app.post("/Chipper/Pet/Add", pet.addPet)
+app.delete("/Chipper/Pet/Remove/:pet_id", pet.deletePet)
+app.put("/Chipper/Pet/Edit/:pet_id", pet.editPet)
+
+//walker
+app.get("/Chipper/Walker/Application/:application_id", walker.applicationDetails)
+app.get("/Chipper/Walker/Applications/Pending", walker.allApplications)
+app.get("/Chipper/Walker/Applications/Approved", walker.allAccepted)
+app.post("/Chipper/Walker/Applications/Submitted", walker.applyWalker)
+app.delete("/Chipper/Walker/Application/Deny/:application_id", walker.denyWalker)
+app.put("/Chipper/Walker/Application/Approve/:application_id", walker.acceptWalker)
 
 app.listen(6942, () => console.log("Port 6942"))
