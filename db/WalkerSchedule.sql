56a67d95138c218a3f19a9814e8f9b8f7ac2b3d8
Kyle Carter <kkcarter1129@gmail.com>
2019-12-11 12:39:21 -0600

    walker dash functionality done

--- db/WalkerSchedule.sql
+++ db/WalkerSchedule.sql
@@ -1,3 +1,4 @@
-SELECT * FROM jobs
-WHERE walker_id = $1
-ORDER BY year ASC, month ASC, date ASC, time ASC
+SELECT J.job_id, J.pet_id, J.price, J.jobaccepted, J.notes, J.month, J.date, J.year, J.time, J.walker_id, J.owner_id, P.name, P.breed, P.age, P.img
+FROM jobs J 
+INNER JOIN pets AS P ON P.pet_id = J.pet_id 
+WHERE J.walker_id = $1
