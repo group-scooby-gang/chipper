e9fbe8a0c75c64aaeec60ac0e1f7833c78a29823
Kyle Carter <kkcarter1129@gmail.com>
2019-12-11 14:35:51 -0600

    working on scheduling a new walk

---
+++ src/components/schedule/owner/scheduleNewWalk/walkerSelect/scheduleNewWalkWalker.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './scheduleNewWalkWalker.css';
+
+class WalkerSelect extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <h2>Mapped Walker</h2>
+                    <h2>Mapped Walker</h2>
+                    <h2>Mapped Walker</h2>
+                </div>
+                <div>
+                    <select name="state">
+                        <option value="State">State</option>
+                    </select>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default WalkerSelect;
