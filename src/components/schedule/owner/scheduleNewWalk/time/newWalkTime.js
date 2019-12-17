9fc10a66b87deeb2105e74940e6f324983f677a2
HAL_9000 <jessebadgley@HAL-9000.local>
2019-12-17 13:16:08 -0600

    updating style

--- src/components/schedule/owner/scheduleNewWalk/time/newWalkTime.js
+++ src/components/schedule/owner/scheduleNewWalk/time/newWalkTime.js
@@ -1,58 +1,88 @@
 import React, { Component } from 'react';
 import './newWalkTime.css';
-import { updateState, getWalkerById } from './../../../../../redux/ownerReducer';
+import {
+	updateState,
+	getWalkerById
+} from './../../../../../redux/ownerReducer';
 import { connect } from 'react-redux';
 
 class WalkTime extends Component {
+	componentDidMount() {
+		this.props.getWalkerById(this.props.selectedWalker);
+	}
 
-    componentDidMount() {
-        this.props.getWalkerById(this.props.selectedWalker)
-    }
+	next = () => {
+		this.props.history.push('/owner/schedule/new/extra_notes');
+	};
 
-    next = () => {
-        this.props.history.push('/owner/schedule/new/extra_notes')
-    }
+	back = () => {
+		this.props.history.goBack();
+	};
 
-    back = () => {
-        this.props.history.goBack()
-    }
+	handleChange = (e) => {
+		this.props.updateState({ [e.target.name]: parseInt(e.target.value) });
+	};
 
-    handleChange = e => {
-        this.props.updateState({ [e.target.name]: parseInt(e.target.value) })
-    }
-
-    render() {
-        const { _15minprice, _30minprice, _45minprice, _60minprice } = this.props;
-        return (
-            <div className='time_selection_page'>
-                    <h1>Time</h1>
-                <div className='selection_container'>
-                    <input onChange={this.handleChange} type="radio" name='payment' value={_15minprice} /> 15 min. (${_15minprice})<br />
-                    <input onChange={this.handleChange} type="radio" name='payment' value={_30minprice} /> 30 min. (${_30minprice})<br />
-                    <input onChange={this.handleChange} type="radio" name='payment' value={_45minprice} /> 45 min. (${_45minprice})<br />
-                    <input onChange={this.handleChange} type="radio" name='payment' value={_60minprice} /> 60 min. (${_60minprice})<br />
-                </div>
-                <button onClick={this.next}>Next</button>
-                <button onClick={this.back}>Back</button>
-                <div>Status Bar</div>
-            </div>
-        )
-    }
+	render() {
+		const { _15minprice, _30minprice, _45minprice, _60minprice } = this.props;
+		return (
+			<div className='time_selection_page'>
+				<h1>Time</h1>
+				<div className='selection_container'>
+					<input
+						onChange={this.handleChange}
+						type='radio'
+						name='payment'
+						value={_15minprice}
+					/>{' '}
+					15 min. (${_15minprice})<br />
+					<input
+						onChange={this.handleChange}
+						type='radio'
+						name='payment'
+						value={_30minprice}
+					/>{' '}
+					30 min. (${_30minprice})<br />
+					<input
+						onChange={this.handleChange}
+						type='radio'
+						name='payment'
+						value={_45minprice}
+					/>{' '}
+					45 min. (${_45minprice})<br />
+					<input
+						onChange={this.handleChange}
+						type='radio'
+						name='payment'
+						value={_60minprice}
+					/>{' '}
+					60 min. (${_60minprice})<br />
+				</div>
+				<button className='time_button_next' onClick={this.next}>
+					NEXT
+				</button>
+				<button className='time_button_back' onClick={this.back}>
+					BACK
+				</button>
+				<div>Status Bar</div>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-        payment: state.ownerReducer.payment,
-        selectedWalker: state.ownerReducer.selectedWalker,
-        _15minprice: state.ownerReducer._15minprice,
-        _30minprice: state.ownerReducer._30minprice,
-        _45minprice: state.ownerReducer._45minprice,
-        _60minprice: state.ownerReducer._60minprice,
-        time: state.ownerReducer.time
-    }
-}
+const mapStateToProps = (state) => {
+	return {
+		payment: state.ownerReducer.payment,
+		selectedWalker: state.ownerReducer.selectedWalker,
+		_15minprice: state.ownerReducer._15minprice,
+		_30minprice: state.ownerReducer._30minprice,
+		_45minprice: state.ownerReducer._45minprice,
+		_60minprice: state.ownerReducer._60minprice,
+		time: state.ownerReducer.time
+	};
+};
 
 export default connect(mapStateToProps, {
-    updateState,
-    getWalkerById
+	updateState,
+	getWalkerById
 })(WalkTime);
